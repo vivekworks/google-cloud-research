ba89cf665a8c1c197764351e6a0d55b21482ce37
Vivek T S <vivekts.dev@gmail.com>
2022-02-14 00:20:57 +0530

    logback

--- java-error-reporting/src/main/java/dev/vivekts/jer/JERController.java
+++ java-error-reporting/src/main/java/dev/vivekts/jer/JERController.java
@@ -3,39 +3,29 @@ package dev.vivekts.jer;
 import com.google.cloud.MonitoredResource;
 import com.google.cloud.logging.*;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
-
-import java.util.Collections;
-import java.util.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/jer")
 public class JERController {
 
-    private static final Logger LOGGER = Logger.getLogger(JERController.class.getName());
+    private static final Logger LOGGER = LoggerFactory.getLogger(JERController.class);
 
 
     @GetMapping("/{value}")
-    public Mono<ResponseEntity<String>> check(@PathVariable int value) throws Exception {
+    public ResponseEntity<String> check(@PathVariable int value) throws Exception {
         if (value % 2 == 0) {
-            return Mono.just(ResponseEntity.ok("Everything's alright!!!"));
+            return ResponseEntity.ok("Everything's alright!!!");
         } else {
-            try (Logging logging = LoggingOptions.getDefaultInstance().getService()) {
-                String textPayload = "Reporting error because of value " + value;
-                LogEntry entry =
-                        LogEntry.newBuilder(Payload.StringPayload.of(textPayload))
-                                .setSeverity(Severity.ERROR)
-                                .setLogName(LOGGER.getName())
-                                .setResource(MonitoredResource.newBuilder("global").build())
-                                .build();
-                logging.write(Collections.singleton(entry));
-            }
-            return Mono.just(ResponseEntity.ok("Reporting Error!!!"));
+            LOGGER.error("Reporting Error on JERController " + value);
+            return ResponseEntity.ok("Reporting Error!!!");
         }
     }
 }
--- java-error-reporting/src/main/java/dev/vivekts/jer/JavaErrorReportingApplication.java
+++ java-error-reporting/src/main/java/dev/vivekts/jer/JavaErrorReportingApplication.java
@@ -1,12 +1,17 @@
 package dev.vivekts.jer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavaErrorReportingApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(JavaErrorReportingApplication.class);
+
 	public static void main(String[] args) {
+		LOGGER.error("Logging ERROR with JavaErrorReportingApplication");
 		SpringApplication.run(JavaErrorReportingApplication.class, args);
 	}
 
