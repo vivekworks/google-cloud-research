e6aa512b334b24ccdcfa1cdebe8c08bff471c3fd
Vivek T S <vivekts.dev@gmail.com>
2022-02-13 22:12:10 +0530

    Adding error reporting logs

--- java-error-reporting/src/main/java/dev/vivekts/jer/JERController.java
+++ java-error-reporting/src/main/java/dev/vivekts/jer/JERController.java
@@ -1,5 +1,7 @@
 package dev.vivekts.jer;
 
+import com.google.cloud.MonitoredResource;
+import com.google.cloud.logging.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,17 +10,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.util.Collections;
+import java.util.logging.Logger;
+
 @RestController
 @RequestMapping("/jer")
-@Slf4j
 public class JERController {
 
+    private static final Logger LOGGER = Logger.getLogger(JERController.class.getName());
+
+
     @GetMapping("/{value}")
-    public Mono<ResponseEntity<String>> check(@PathVariable int value) {
+    public Mono<ResponseEntity<String>> check(@PathVariable int value) throws Exception {
         if (value % 2 == 0) {
             return Mono.just(ResponseEntity.ok("Everything's alright!!!"));
         } else {
-            log.error("Reporting Error ", new IllegalArgumentException("Value " + value));
+            try (Logging logging = LoggingOptions.getDefaultInstance().getService()) {
+                String textPayload = "Reporting error because of value " + value;
+                LogEntry entry =
+                        LogEntry.newBuilder(Payload.StringPayload.of(textPayload))
+                                .setSeverity(Severity.ERROR)
+                                .setLogName(LOGGER.getName())
+                                .setResource(MonitoredResource.newBuilder("global").build())
+                                .build();
+                logging.write(Collections.singleton(entry));
+            }
             return Mono.just(ResponseEntity.ok("Reporting Error!!!"));
         }
     }
